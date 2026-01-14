cf726400cddb97cee92f7666429bf8e042220e8f
adithya8078 <adithyachandrats@gmail.com>
2026-01-14 21:18:44 +0530

    Initial commit

---
+++ qaiz-portfolio/src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Services from "../components/Services";
+// import Portfolio from "../components/Portfolio";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Skills />
+        <Services />
+        {/* <Portfolio /> */}
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
